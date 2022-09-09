a5009042e99a4d223add922d3460ebbf10abedd7
Tolu Abolade <mightbeq@gmail.com>
2022-09-09 10:16:17 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lowabc;
+
+	for (lowabc = 'a'; lowabc <= 'z'; lowabc++)
+	{
+		if (lowabc != 'q' && lowabc != 'e'
+				putchar(lowabc);
+	}
+	putchar('\n');
+	return (0);
+}
