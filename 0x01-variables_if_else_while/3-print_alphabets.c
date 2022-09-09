92752a058deb1d403b7539c24e5f8e57b794bc80
Tolu Abolade <mightbeq@gmail.com>
2022-09-09 10:07:01 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lowabc;
+
+	for (lowabc = 'a'; lowabc <= 'z'; lowabc++)
+	{
+		putchar(lowabc);
+	}
+	for (upabc = 'a'; upabc <= 'z'; upabc++)
+	{
+		putchar(upabc);
+	}
+	putchar('\n');
+	return (0);
+}
+
