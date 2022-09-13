3db9c217421ba50ce457719cf4a9db8cb5b817a1
Tolu Abolade <mightbeq@gmail.com>
2022-09-13 09:42:42 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: nothing.
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
